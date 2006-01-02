95d53630f44dcbbba3e3b70555d32f9c41396c10
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2006-01-02 20:35:02 +0000

    refactored sfPropelData
    git-svn-id: http://svn.symfony-project.com/branches/fabien-constants@312 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/symfony/addon/propel/sfPropelData.class.php
+++ lib/symfony/addon/propel/sfPropelData.class.php
@@ -20,7 +20,9 @@ require_once('pake/pakeFinder.class.php');
 class sfPropelData
 {
   private
-    $deleteCurrentData = true;
+    $deleteCurrentData = true,
+    $maps              = array(),
+    $object_references = array();
 
   public function setDeleteCurrentData($boolean)
   {
@@ -32,10 +34,139 @@ class sfPropelData
     return $this->deleteCurrentData;
   }
 
-  // data/ -> name voir c2 test en tableaux
-  // symfony load-data (file|dir) (option pour supprimer)
+  // symfony load-data (file|dir)
   // todo: symfony dump-data
   public function loadData($directory_or_file = null)
+  {
+    $fixture_files = $this->getFiles($directory_or_file);
+
+    // wrap all databases operations in a single transaction
+    $con = Propel::getConnection();
+    try
+    {
+      $con->begin();
+
+      $this->doDeleteCurrentData($fixture_files);
+
+      $this->doLoadData($fixture_files);
+
+      $con->commit();
+    }
+    catch (Exception $e)
+    {
+      $con->rollback();
+      throw $e;
+    }
+  }
+
+  protected function doLoadDataFromFile($fixture_file)
+  {
+    // import new datas
+    $main_datas = sfYaml::load($fixture_file);
+    foreach ($main_datas as $class => $datas)
+    {
+      $class = trim($class);
+
+      $peer_class = $class.'Peer';
+
+      // load map class
+      $this->loadMapBuilder($class);
+
+      $tableMap = $this->maps[$class]->getDatabaseMap()->getTable(constant($peer_class.'::TABLE_NAME'));
+
+      $column_names = call_user_func_array(array($peer_class, 'getFieldNames'), array(BasePeer::TYPE_FIELDNAME));
+
+      // iterate through datas for this class
+      foreach ($datas as $key => $data)
+      {
+        // create a new entry in the database
+        $obj = new $class();
+        foreach ($data as $name => $value)
+        {
+          // foreign key?
+          try
+          {
+            $column = $tableMap->getColumn($name);
+            if ($column->isForeignKey())
+            {
+              $relatedTable = $this->maps[$class]->getDatabaseMap()->getTable($column->getRelatedTableName());
+              if (!isset($this->object_references[$relatedTable->getPhpName().'_'.$value]))
+              {
+                $error = 'The object "%s" from class "%s" is not defined in your data file.';
+                $error = sprintf($error, $value, $relatedTable->getPhpName());
+                throw new sfException($error);
+              }
+              $value = $this->object_references[$relatedTable->getPhpName().'_'.$value];
+            }
+          }
+          catch (PropelException $e)
+          {
+          }
+
+          $pos = array_search($name, $column_names);
+          $method = 'set'.sfInflector::camelize($name);
+          if ($pos)
+          {
+            $obj->setByPosition($pos, $value);
+          }
+          else if (method_exists($obj, $method))
+          {
+            $obj->$method($value);
+          }
+          else
+          {
+            $error = 'Column "%s" does not exist for class "%s"';
+            $error = sprintf($error, $name, $class);
+            throw new sfException($error);
+          }
+        }
+        $obj->save();
+
+        // save the id for future reference
+        if (method_exists($obj, 'getPrimaryKey'))
+        {
+          $this->object_references[$class.'_'.$key] = $obj->getPrimaryKey();
+        }
+      }
+    }
+  }
+
+  protected function doLoadData($fixture_files)
+  {
+    $this->object_references = array();
+    $this->maps = array();
+
+    sort($fixture_files);
+    foreach ($fixture_files as $fixture_file)
+    {
+      $this->doLoadDataFromFile($fixture_file);
+    }
+  }
+
+  protected function doDeleteCurrentData($fixture_files)
+  {
+    // delete all current datas in database
+    if ($this->deleteCurrentData)
+    {
+      rsort($fixture_files);
+      foreach ($fixture_files as $fixture_file)
+      {
+        $main_datas = sfYaml::load($fixture_file);
+        $classes = array_keys($main_datas);
+        krsort($classes);
+        foreach ($classes as $class)
+        {
+          $peer_class = trim($class.'Peer');
+
+          require_once('model/'.$peer_class.'.php');
+
+          call_user_func(array($peer_class, 'doDeleteAll'));
+        }
+      }
+    }
+  }
+
+  protected function getFiles($directory_or_file = null)
   {
     // directory or file?
     $fixture_files = array();
@@ -54,100 +185,20 @@ class sfPropelData
     }
     else
     {
-      throw new sfInitializationException('You must give a directory or file.');
+      throw new sfInitializationException('You must give a directory or a file.');
     }
 
-    $objects = array();
+    return $fixture_files;
+  }
 
-    // delete all current datas in database
-    if ($this->deleteCurrentData)
+  private function loadMapBuilder($class)
+  {
+    $class_map_builder = $class.'MapBuilder';
+    if (!isset($this->maps[$class]))
     {
-      rsort($fixture_files);
-      foreach ($fixture_files as $fixture_file)
-      {
-        $main_datas = sfYaml::load($fixture_file);
-        $classes = array_keys($main_datas);
-        krsort($classes);
-        foreach ($classes as $class)
-        {
-          $peer_class = trim($class.'Peer');
-
-          call_user_func(array($peer_class, 'doDeleteAll'));
-        }
-      }
-    }
-
-    sort($fixture_files);
-    $maps = array();
-    foreach ($fixture_files as $fixture_file)
-    {
-      // import new datas
-      $main_datas = sfYaml::load($fixture_file);
-      foreach ($main_datas as $class => $datas)
-      {
-        $class = trim($class);
-
-        $peer_class = $class.'Peer';
-
-        // load map class
-        $class_map_builder = $class.'MapBuilder';
-        require_once('model/map/'.$class_map_builder.'.php');
-        if (!isset($maps[$class]))
-        {
-          $maps[$class] = new $class_map_builder();
-          $maps[$class]->doBuild();
-        }
-        $tableMap = $maps[$class]->getDatabaseMap()->getTable(constant($peer_class.'::TABLE_NAME'));
-
-        $column_names = call_user_func_array(array($peer_class, 'getFieldNames'), array(BasePeer::TYPE_FIELDNAME));
-
-        // iterate through datas for this class
-        foreach ($datas as $key => $data)
-        {
-          // create a new entry in the database
-          $obj = new $class();
-          foreach ($data as $name => $value)
-          {
-            // foreign key?
-            try
-            {
-              $column = $tableMap->getColumn($name);
-              if ($column->isForeignKey())
-              {
-                $relatedTable = $maps[$class]->getDatabaseMap()->getTable($column->getRelatedTableName());
-                $value = $objects[$relatedTable->getPhpName().'_'.$value];
-              }
-            }
-            catch (PropelException $e)
-            {
-            }
-
-            $pos = array_search($name, $column_names);
-            $method = 'set'.sfInflector::camelize($name);
-            if ($pos)
-            {
-              $obj->setByPosition($pos, $value);
-            }
-            else if (method_exists($obj, $method))
-            {
-              $obj->$method($value);
-            }
-            else
-            {
-              $error = 'Column "%s" does not exist for class "%s"';
-              $error = sprintf($error, $name, $class);
-              throw new sfException($error);
-            }
-          }
-          $obj->save();
-
-          // save the id for future reference
-          if (method_exists($obj, 'getPrimaryKey'))
-          {
-            $objects[$class.'_'.$key] = $obj->getPrimaryKey();
-          }
-        }
-      }
+      require_once('model/map/'.$class_map_builder.'.php');
+      $this->maps[$class] = new $class_map_builder();
+      $this->maps[$class]->doBuild();
     }
   }
 }
