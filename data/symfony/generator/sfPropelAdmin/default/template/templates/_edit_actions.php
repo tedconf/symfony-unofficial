ba391891ed2dab524e129e0962faf4066217af2c
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2006-01-04 11:12:31 +0000

    changed the look of customized actions for admin generator edit
    git-svn-id: http://svn.symfony-project.com/branches/fabien-constants@323 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/symfony/generator/sfPropelAdmin/default/template/templates/_edit_actions.php
+++ data/symfony/generator/sfPropelAdmin/default/template/templates/_edit_actions.php
@@ -3,11 +3,12 @@
 [?php endif ?]
 <?php foreach ($this->getParameterValue('edit.actions') as $actionName => $params): ?>
   <?php
-    $name   = isset($params['name']) ? $params['name'] : $actionName;
-    $icon   = isset($params['icon']) ? $params['icon'] : '/sf/images/sf_admin/default_icon.png';
-    $action = isset($params['action']) ? $params['action'] : 'List'.sfInflector::camelize($actionName);
+    $name    = isset($params['name']) ? $params['name'] : $actionName;
+    $icon    = isset($params['icon']) ? $params['icon'] : '/sf/images/sf_admin/default_icon.png';
+    $action  = isset($params['action']) ? $params['action'] : 'List'.sfInflector::camelize($actionName);
+    $options = isset($params['class']) ? 'class='.$params['class'] : 'style=background: #ffc url('.$icon.') no-repeat 3px 2px';
   ?>
-  <li>[?php echo link_to(image_tag('<?php echo $icon ?>', array('alt' => __('<?php echo $name ?>'), 'title' => __('<?php echo $name ?>'))), '<?php echo $this->getModuleName() ?>/<?php echo $action ?>?<?php echo $this->getPrimaryKeyUrlParams() ?>) ?]</li>
+  <li>[?php echo button_to(__('<?php echo $name ?>'), '<?php echo $this->getModuleName() ?>/<?php echo $action ?>?<?php echo $this->getPrimaryKeyUrlParams() ?>, '<?php echo $options ?>') ?]</li>
 <?php endforeach ?>
   <li>[?php echo button_to(__('cancel'), '<?php echo $this->getModuleName() ?>/list', 'class=sf_admin_cancel') ?]</li>
   <li>[?php echo submit_tag(__('save'), 'class=sf_admin_default_action sf_admin_save') ?]</li>
