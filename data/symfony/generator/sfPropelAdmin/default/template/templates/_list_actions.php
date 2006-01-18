eb0957b48ac01364b3adf29f78fb6ecf83deff2a
rovert <rovert@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2006-01-18 18:39:49 +0000

    RoVeRT: remove empty directories
    git-svn-id: http://svn.symfony-project.com/branches/rovert@467 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/symfony/generator/sfPropelAdmin/default/template/templates/_list_actions.php
+++
@@ -1,9 +0,0 @@
-<ul class="sf_admin_actions">
-<?php if ($this->getParameterValue('list.actions')): ?>
-<?php foreach ($this->getParameterValue('list.actions') as $actionName => $params): ?>
-  <?php echo $this->getButtonToAction($actionName, $params, false) ?>
-<?php endforeach ?>
-<?php else: ?>
-  <?php echo $this->getButtonToAction('_create', array(), false) ?>
-<?php endif ?>
-</ul>
