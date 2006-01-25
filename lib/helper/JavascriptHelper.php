26df7282efe983a2a080fa643961b27d087dc6da
rovert <rovert@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2006-01-25 04:06:16 +0000

    RoVeRT: fixed evalScript:true in JavascriptHelper.php
    git-svn-id: http://svn.symfony-project.com/branches/rovert@551 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/helper/JavascriptHelper.php
+++ lib/helper/JavascriptHelper.php
@@ -907,7 +907,7 @@
     $js_options['asynchronous'] = (isset($options['type'])) ? ($options['type'] != 'synchronous') : 'true';
     if (isset($options['method'])) $js_options['method'] = _method_option_to_s($options['method']);
     if (isset($options['insertion'])) $js_options['insertion'] = "Insertion.".sfInflector::camelize($options['position']);
-    $js_options['evalScripts'] = (!isset($options['script']) || $options['script'] == 'false') ? 'false' : 'true';
+    $js_options['evalScripts'] = (isset($options['script']) && ($options['script'] != 0 || $options['script'] != 'false')) ? 'true' : 'false';
 
     if (isset($options['form']))
     {
