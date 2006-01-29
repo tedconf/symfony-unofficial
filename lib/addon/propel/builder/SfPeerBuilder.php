3531e51bcccc246d07a72d5c66737d69859e93ac
rovert <rovert@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2006-01-29 14:18:05 +0000

    RoVeRT: make PropelObject->Validate() add errors to symfony under PhpName
    git-svn-id: http://svn.symfony-project.com/branches/rovert@619 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/addon/propel/builder/SfPeerBuilder.php
+++ lib/addon/propel/builder/SfPeerBuilder.php
@@ -89,7 +89,7 @@ class sfPeerBuilder extends PHP5ComplexPeerBuilder
               $script .= "
       \$omClass = ".$this->getPeerClassname()."::getOMClass();
 ";
-            } 
+            }
             $script .= "
       \$cls = Propel::import(\$omClass);
       \$obj1 = new \$cls();
@@ -99,7 +99,7 @@ class sfPeerBuilder extends PHP5ComplexPeerBuilder
               $script .= "
       \$omClass = ".$i18nTablePeerBuilder->getPeerClassname()."::getOMClass(\$rs, \$startcol);
 ";
-//            } else { 
+//            } else {
 //              $script .= "
 //      \$omClass = ".$i18nTablePeerBuilder->getPeerClassname()."::getOMClass();
 //";
@@ -118,6 +118,22 @@ class sfPeerBuilder extends PHP5ComplexPeerBuilder
     return \$results;
   }
 ";
+  }
+
+  public function addDoValidate(&$script) {
+	    $tmp = '';
+      parent::addDoValidate($tmp);
+
+      $script .= str_replace("return {$this->basePeerClassname}::doValidate(".$this->getPeerClassname()."::DATABASE_NAME, ".$this->getPeerClassname()."::TABLE_NAME, \$columns);\n",
+        "\$res =  {$this->basePeerClassname}::doValidate(".$this->getPeerClassname()."::DATABASE_NAME, ".$this->getPeerClassname()."::TABLE_NAME, \$columns);\n".
+        "    if (\$res !== true) {\n".
+        "        \$request = sfContext::getInstance()->getRequest();\n".
+        "        foreach (\$res as \$failed) {\n".
+        "            \$col = ".$this->getPeerClassname()."::translateFieldname(\$failed->getColumn(), BasePeer::TYPE_COLNAME, BasePeer::TYPE_PHPNAME);\n".
+        "            \$request->setError(\$col, \$failed->getMessage());\n".
+        "        }\n".
+        "    }\n\n".
+        "    return \$res;\n", $tmp);
   }
 }
 
