f423c4c157b200ad5bda3207dd607bb2839045b5
pookey <pookey@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2006-04-06 11:58:49 +0000

    pookey - anchoring the email validation regex
    git-svn-id: http://svn.symfony-project.com/branches/pookey@1127 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/validator/sfEmailValidator.class.php
+++ lib/validator/sfEmailValidator.class.php
@@ -52,7 +52,7 @@ class sfEmailValidator extends sfValidator
      *   $local_part = "$word(\\x2e$word)*";
      *   $addr_spec = "$local_part\\x40$domain";
      */
-    $re = '/([^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-'
+    $re = '/^([^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-'
         . '\\x5d\\x7f-\\xff]+|\\x22([^\\x0d\\x22\\x5c\\x80-\\xff]|\\x5c\\x00-'
         . '\\x7f)*\\x22)(\\x2e([^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-'
         . '\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+|\\x22([^\\x0d\\x22\\x5c\\x80'
@@ -61,7 +61,7 @@ class sfEmailValidator extends sfValidator
         . '\\x0d\\x5b-\\x5d\\x80-\\xff]|\\x5c\\x00-\\x7f)*\\x5d)(\\x2e([^\\x00-'
         . '\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-'
         . '\\xff]+|\\x5b([^\\x0d\\x5b-\\x5d\\x80-\\xff]|\\x5c\\x00-\\x7f)*'
-        . '\\x5d))*/'
+        . '\\x5d))*$/'
     ;
 
     if (!preg_match($re, $value))
