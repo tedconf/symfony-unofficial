7fff9142414dbdd5107f160b8324762423352de2
dwhittle <dwhittle@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2006-06-29 03:17:09 +0000

    dwhittle: updated deprecated function call (closes #681)
    git-svn-id: http://svn.symfony-project.com/branches/dwhittle@1543 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/helper/ObjectAdminHelper.php
+++ lib/helper/ObjectAdminHelper.php
@@ -5,7 +5,7 @@ require_once(sfConfig::get('sf_symfony_lib_dir').'/helper/FormHelper.php');
 /*
  * This file is part of the symfony package.
  * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -46,7 +46,7 @@ function object_admin_input_upload_tag($object, $method, $options = array())
   unset($options['include_text']);
   unset($options['include_remove']);
 
-  return input_upload_tag($name, $options)."\n<br />".$html;
+  return input_file_tag($name, $options)."\n<br />".$html;
 }
 
 function object_edit_collection($object, $method, $options = array())
