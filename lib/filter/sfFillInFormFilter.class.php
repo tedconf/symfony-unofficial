fcd0a7bea3c347d294712c412d9cc1943c52f4e1
synace <synace@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2006-09-14 18:19:56 +0000

    -
    git-svn-id: http://svn.symfony-project.com/branches/synace@2090 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/filter/sfFillInFormFilter.class.php
@@ -0,0 +1,165 @@
+<?php
+
+/*
+ * This file is part of the symfony package.
+ * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ *
+ * @package    symfony
+ * @subpackage filter
+ * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
+ * @version    SVN: $Id$
+ */
+class sfFillInFormFilter extends sfFilter
+{
+  private $escapers = array();
+
+  public function executeBeforeRendering($filterChain)
+  {
+    $context  = $this->getContext();
+    $response = $context->getResponse();
+    $request  = $context->getRequest();
+
+    $doc = new DomDocument('1.0', sfConfig::get('sf_charset'));
+    @$doc->loadHTML($response->getContent());
+    $xpath = new DomXPath($doc);
+
+    // load converters
+    foreach ($this->getParameter('converters', array()) as $functionName => $parameters)
+    {
+      if (!is_array($parameters))
+      {
+        $parameters = array($parameters);
+      }
+
+      foreach ($parameters as $parameter)
+      {
+        $this->escapers[$parameter][] = $functionName;
+      }
+    }
+
+    $skip_fields = $this->getParameter('skip_fields', array());
+
+    $exclude_types = $this->getParameter('exclude_types', array('hidden', 'password'));
+    $check_types   = $this->getParameter('check_types',   array('text', 'checkbox', 'radio', 'password', 'hidden'));
+    $types = array_diff($check_types, $exclude_types);
+    $query = 'descendant::input[@name and (not(@type)';
+    foreach ($types as $type)
+    {
+      $query .= ' or @type="'.$type.'"';
+    }
+    $query .= ')] | descendant::textarea[@name] | descendant::select[@name]';
+
+    // find our form
+    $xpath_query = $this->getParameter('name') ? '//form[@name="'.$this->getParameter('name').'"]' : '//form';
+    if ($form = $xpath->query($xpath_query)->item(0))
+    {
+      $filledFields = array();
+
+      foreach ($xpath->query($query, $form) as $element)
+      {
+        // skip fields specified in the 'skip_fields' attribute
+        if ($request->hasParameter($element->getAttribute('name')) && in_array($element->getAttribute('name'), $skip_fields))
+        {
+          continue;
+        }
+
+        $filledFields[] = $element->getAttribute('name');
+
+        if ($element->nodeName == 'input')
+        {
+          if ($element->getAttribute('type') == 'checkbox' || $element->getAttribute('type') == 'radio')
+          {
+            // checkbox and radio
+            $name = $element->getAttribute('name');
+            $element->removeAttribute('checked');
+            if ($request->hasParameter($name) && ($request->getParameter($name) == $element->getAttribute('value') || !$element->hasAttribute('value')))
+            {
+              $element->setAttribute('checked', 'checked');
+            }
+          }
+          else
+          {
+            // text input
+            $element->removeAttribute('value');
+            if ($request->hasParameter($element->getAttribute('name')))
+            {
+              $element->setAttribute('value', $this->escapeRequestParameter($request, $element->getAttribute('name')));
+            }
+          }
+        }
+        else if ($element->nodeName == 'textarea')
+        {
+          foreach ($element->childNodes as $child_node)
+          {
+            $element->removeChild($child_node);
+          }
+          $element->appendChild($doc->createTextNode($this->escapeRequestParameter($request, $element->getAttribute('name'))));
+        }
+        else if ($element->nodeName == 'select')
+        {
+          // select
+          $name     = $element->getAttribute('name');
+          $value    = $request->getParameter($name);
+          $multiple = $element->hasAttribute('multiple');
+          foreach ($xpath->query('descendant::option', $element) as $option)
+          {
+            $option->removeAttribute('selected');
+            if ($multiple && is_array($value))
+            {
+              if (in_array($option->getAttribute('value'), $value))
+              {
+                $option->setAttribute('selected', 'selected');
+              }
+            }
+            else if ($value == $option->getAttribute('value'))
+            {
+              $option->setAttribute('selected', 'selected');
+            }
+          }
+        }
+      } // foreach
+
+      if (sfConfig::get('sf_logging_active'))
+      {
+        $context->getLogger()->info(sprintf('{sfFillInFilter} Filled the following fields: %s', implode(', ', $filledFields)));
+      }
+    }
+
+    $response->setContent($doc->saveHTML());
+
+    unset($doc);
+
+    // execute next filter
+    $filterChain->execute();
+  }
+
+  private function escapeRequestParameter($request, $name)
+  {
+    $value = $request->getParameter($name);
+
+    if (extension_loaded('iconv') && strtolower(sfConfig::get('sf_charset')) != 'utf-8')
+    {
+      $new_value = iconv(sfConfig::get('sf_charset'), 'UTF-8', $value);
+      if (false !== $new_value)
+      {
+        $value = $new_value;
+      }
+    }
+
+    if (isset($this->escapers[$name]))
+    {
+      foreach ($this->escapers[$name] as $function)
+      {
+        $value = $function($value);
+      }
+    }
+
+    return $value;
+  }
+}
---
+++ lib/response/sfWebResponse.class.php
@@ -0,0 +1,528 @@
+<?php
+
+/*
+ * This file is part of the symfony package.
+ * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * sfWebResponse class.
+ *
+ * This class manages web reponses. It supports cookies and headers management.
+ * 
+ * @package    symfony
+ * @subpackage response
+ * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
+ * @version    SVN: $Id$
+ */
+class sfWebResponse extends sfResponse
+{
+  private
+    $cookies    = array(),
+    $headers    = array(),
+    $statusCode = 200,
+    $statusText = 'OK',
+    $statusTexts = array();
+
+  /**
+   * Initialize this sfWebResponse.
+   *
+   * @param sfContext A sfContext instance.
+   *
+   * @return bool true, if initialization completes successfully, otherwise false.
+   *
+   * @throws <b>sfInitializationException</b> If an error occurs while initializing this Response.
+   */
+  public function initialize ($context, $parameters = array())
+  {
+    parent::initialize($context, $parameters);
+
+    $this->statusTexts = array(
+      '100' => 'Continue',
+      '101' => 'Switching Protocols',
+      '200' => 'OK',
+      '201' => 'Created',
+      '202' => 'Accepted',
+      '203' => 'Non-Authoritative Information',
+      '204' => 'No Content',
+      '205' => 'Reset Content',
+      '206' => 'Partial Content',
+      '300' => 'Multiple Choices',
+      '301' => 'Moved Permanently',
+      '302' => 'Found',
+      '303' => 'See Other',
+      '304' => 'Not Modified',
+      '305' => 'Use Proxy',
+      '306' => '(Unused)',
+      '307' => 'Temporary Redirect',
+      '400' => 'Bad Request',
+      '401' => 'Unauthorized',
+      '402' => 'Payment Required',
+      '403' => 'Forbidden',
+      '404' => 'Not Found',
+      '405' => 'Method Not Allowed',
+      '406' => 'Not Acceptable',
+      '407' => 'Proxy Authentication Required',
+      '408' => 'Request Timeout',
+      '409' => 'Conflict',
+      '410' => 'Gone',
+      '411' => 'Length Required',
+      '412' => 'Precondition Failed',
+      '413' => 'Request Entity Too Large',
+      '414' => 'Request-URI Too Long',
+      '415' => 'Unsupported Media Type',
+      '416' => 'Requested Range Not Satisfiable',
+      '417' => 'Expectation Failed',
+      '500' => 'Internal Server Error',
+      '501' => 'Not Implemented',
+      '502' => 'Bad Gateway',
+      '503' => 'Service Unavailable',
+      '504' => 'Gateway Timeout',
+      '505' => 'HTTP Version Not Supported',
+    );
+  }
+
+  /**
+   * Set a cookie.
+   *
+   * @param string HTTP header name
+   * @param string value
+   *
+   * @return void
+   */
+  public function setCookie ($name, $value, $expire = null, $path = '/', $domain = '', $secure = false, $httpOnly = false)
+  {
+    if ($expire !== null)
+    {
+      if (is_numeric($expire))
+      {
+        $expire = (int) $expire;
+      }
+      else
+      {
+        $expire = strtotime($expire);
+        if ($expire === false || $expire == -1)
+        {
+          throw new sfException('Your expire parameter is not valid.');
+        }
+      }
+    }
+
+    $this->cookies[] = array(
+      'name'     => $name,
+      'value'    => $value,
+      'expire'   => $expire,
+      'path'     => $path,
+      'domain'   => $domain,
+      'secure'   => $secure ? true : false,
+      'httpOnly' => $httpOnly,
+    );
+  }
+
+  /**
+   * Set response status code.
+   *
+   * @param string HTTP status code
+   * @param string HTTP status text
+   *
+   * @return void
+   */
+  public function setStatusCode ($code, $name = null)
+  {
+    $this->statusCode = $code;
+    $this->statusText = $name ? $name : $this->statusTexts[$code];
+  }
+
+  public function getStatusCode ()
+  {
+    return $this->statusCode;
+  }
+
+  /**
+   * Set a HTTP header.
+   *
+   * @param string HTTP header name
+   * @param string value
+   *
+   * @return void
+   */
+  public function setHttpHeader ($name, $value, $replace = true)
+  {
+    $name = $this->normalizeHeaderName($name);
+
+    if ('Content-Type' == $name)
+    {
+      $this->setContentType($value);
+
+      return;
+    }
+
+    $exists = isset($this->headers[$name]);
+
+    if ($exists && !$replace)
+    {
+      return;
+    }
+
+    if (!$exists || $replace)
+    {
+      $this->headers[$name] = array();
+    }
+
+    $this->headers[$name][] = $value;
+  }
+
+  /**
+   * Get HTTP header current value.
+   *
+   * @return array
+   */
+  public function getHttpHeader ($name, $defaultValue = null)
+  {
+    $retval = array($defaultValue);
+
+    if (isset($this->headers[$this->normalizeHeaderName($name)]))
+    {
+      $retval = $this->headers[$this->normalizeHeaderName($name)];
+    }
+
+    return $retval;
+  }
+
+  /**
+   * Set response content type.
+   *
+   * @param string value
+   *
+   * @return void
+   */
+  public function setContentType ($value)
+  {
+    // add charset if needed
+    if (false === stripos($value, 'charset'))
+    {
+      $value .= '; charset='.sfConfig::get('sf_charset');
+    }
+
+    if (isset($this->headers['Content-Type']))
+    {
+      $this->headers['Content-Type'] = array();
+    }
+
+    $this->headers['Content-Type'][] = $value;
+  }
+
+  /**
+   * Get response content type.
+   *
+   * @return array
+   */
+  public function getContentType ()
+  {
+    $ct = $this->getHttpHeader('Content-Type', 'text/html');
+
+    return $ct[0];
+  }
+
+  /**
+   * Has a HTTP header.
+   *
+   * @return boolean
+   */
+  public function hasHttpHeader ($name)
+  {
+    return isset($this->headers[$this->normalizeHeaderName($name)]);
+  }
+
+  /**
+   * Send HTTP headers and cookies.
+   *
+   * @return void
+   */
+  public function sendHttpHeaders ()
+  {
+    // status
+    if (substr(php_sapi_name(), 0, 3) == 'cgi' && isset($_SERVER['SERVER_SOFTWARE']) && false !== stripos($_SERVER['SERVER_SOFTWARE'], 'apache/2'))
+    {
+      // fix bug http://www.symfony-project.com/trac/ticket/669 for apache2/mod_fastcgi
+      $status = 'Status: '.$this->statusCode.' '.$this->statusText;
+    }
+    else
+    {
+      $status = 'HTTP/1.0 '.$this->statusCode.' '.$this->statusText;
+    }
+
+    header($status);
+
+    if (sfConfig::get('sf_logging_active'))
+    {
+      $this->getContext()->getLogger()->info('{sfWebResponse} send status "'.$status.'"');
+    }
+
+    // headers
+    foreach ($this->headers as $name => $values)
+    {
+      foreach ($values as $value)
+      {
+        header($name.': '.$value);
+
+        if (sfConfig::get('sf_logging_active') && $value != '')
+        {
+          $this->getContext()->getLogger()->info('{sfWebResponse} send header "'.$name.'": "'.$value.'"');
+        }
+      }
+    }
+
+    // cookies
+    foreach ($this->cookies as $cookie)
+    {
+      if (version_compare(phpversion(), '5.2', '>='))
+      {
+        setrawcookie($cookie['name'], $cookie['value'], $cookie['expire'], $cookie['path'], $cookie['domain'], $cookie['secure'], $cookie['httpOnly']);
+      }
+      else
+      {
+        setrawcookie($cookie['name'], $cookie['value'], $cookie['expire'], $cookie['path'], $cookie['domain'], $cookie['secure']);
+      }
+
+      if (sfConfig::get('sf_logging_active'))
+      {
+        $this->getContext()->getLogger()->info('{sfWebResponse} send cookie "'.$cookie['name'].'": "'.$cookie['value'].'"');
+      }
+    }
+  }
+
+  private function normalizeHeaderName($name)
+  {
+    if (strtolower($name) == 'etag')
+    {
+      return 'ETag';
+    }
+
+    return preg_replace('/\-(.)/e', "'-'.strtoupper('\\1')", strtr(ucfirst(strtolower($name)), '_', '-'));
+  }
+
+  public function getDate($timestamp, $type = 'rfc1123')
+  {
+    $type = strtolower($type);
+
+    if ($type == 'rfc1123')
+    {
+      return substr(gmdate('r', $timestamp), 0, -5).'GMT';
+    }
+    else if ($type == 'rfc1036')
+    {
+      return gmdate('l, d-M-y H:i:s ', $timestamp).'GMT';
+    }
+    else if ($type == 'asctime')
+    {
+      return gmdate('D M j H:i:s', $timestamp);
+    }
+    else
+    {
+      $error = 'The second getDate() method parameter must be one of: rfc1123, rfc1036 or asctime';
+
+      throw new sfParameterException($error);
+    }
+  }
+
+  public function addVaryHttpHeader($header)
+  {
+    $vary = $this->getHttpHeader('Vary');
+    $currentHeaders = array();
+    if ($vary[0])
+    {
+      $currentHeaders = split('/\s*,\s*/', $vary[0]);
+    }
+    $header = $this->normalizeHeaderName($header);
+
+    if (!in_array($header, $currentHeaders))
+    {
+      $currentHeaders[] = $header;
+      $this->setHttpHeader('Vary', implode(', ', $currentHeaders));
+    }
+  }
+
+  public function addCacheControlHttpHeader($name, $value = null)
+  {
+    $cacheControl = $this->getHttpHeader('Cache-Control');
+    $currentHeaders = array();
+    if ($cacheControl[0])
+    {
+      $currentHeaders = split('/\s*,\s*/', $cacheControl[0]);
+    }
+    $name = strtr(strtolower($name), '_', '-');
+
+    if (!in_array($name, $currentHeaders))
+    {
+      $currentHeaders[] = $name.($value !== null ? '='.$value : '');
+      $this->setHttpHeader('Cache-Control', implode(', ', $currentHeaders));
+    }
+  }
+
+  public function getHttpMetas()
+  {
+    return $this->parameter_holder->getAll('helper/asset/auto/httpmeta');
+  }
+
+  public function addHttpMeta($key, $value, $override = true)
+  {
+    if ($override || !$this->hasParameter($key, 'helper/asset/auto/httpmeta'))
+    {
+      // set HTTP header
+      $this->setHttpHeader($key, $value, false);
+
+      if ('Content-Type' == $this->normalizeHeaderName($key))
+      {
+        $value = $this->getContentType();
+      }
+
+      $this->setParameter($key, $value, 'helper/asset/auto/httpmeta');
+    }
+  }
+
+  public function getMetas()
+  {
+    return $this->parameter_holder->getAll('helper/asset/auto/meta');
+  }
+
+  public function addMeta($key, $value, $override = true, $doNotEscape = false)
+  {
+    if ($override || !$this->hasParameter($key, 'helper/asset/auto/meta'))
+    {
+      if (sfConfig::get('sf_i18n'))
+      {
+        $value = sfConfig::get('sf_i18n_instance')->__($value);
+      }
+
+      if (!$doNotEscape)
+      {
+        $value = htmlentities($value, ENT_QUOTES, sfConfig::get('sf_charset'));
+      }
+
+      $this->setParameter($key, $value, 'helper/asset/auto/meta');
+    }
+  }
+
+  public function getTitle()
+  {
+    $metas = $this->parameter_holder->getAll('helper/asset/auto/meta');
+	
+    return (array_key_exists('title', $metas)) ? $metas['title'] : false;
+  }
+
+  public function setTitle($title, $doNotEscape = false)
+  {
+    if (!$doNotEscape)
+    {
+      if (sfConfig::get('sf_i18n'))
+      {
+        $title = sfConfig::get('sf_i18n_instance')->__($title);
+      }
+
+      $title = htmlentities($title, ENT_QUOTES, sfConfig::get('sf_charset'));
+    }
+
+    $this->setParameter('title', $title, 'helper/asset/auto/meta');
+  }
+
+  public function getStylesheets($position = '')
+  {
+    if ($position)
+    {
+      $position = '/'.$position;
+    }
+
+    return $this->parameter_holder->getAll('helper/asset/auto/stylesheet'.$position);
+  }
+
+  public function addStylesheet($css, $position = '', $options = array())
+  {
+    if ($position)
+    {
+      $position = '/'.$position;
+    }
+
+    $this->setParameter($css, $options, 'helper/asset/auto/stylesheet'.$position);
+  }
+
+  public function getJavascripts($position = '')
+  {
+    if ($position)
+    {
+      $position = '/'.$position;
+    }
+
+    return $this->parameter_holder->getAll('helper/asset/auto/javascript'.$position);
+  }
+
+  public function addJavascript($js, $position = '')
+  {
+    if ($position)
+    {
+      $position = '/'.$position;
+    }
+
+    $this->setParameter($js, $js, 'helper/asset/auto/javascript'.$position);
+  }
+
+  public function getCookies()
+  {
+    $cookies = array();
+    foreach ($this->cookies as $cookie)
+    {
+      $cookies[$cookie['name']] = $cookie;
+    }
+
+    return $cookies;
+  }
+
+  public function getHttpHeaders()
+  {
+    return $this->headers;
+  }
+
+  public function mergeProperties($response)
+  {
+    // view configuration
+    $this->getParameterHolder()->add($response->getParameterHolder()->getAll('symfony/action/view'), 'symfony/action/view');
+
+    // add stylesheets
+    foreach (array('first', '', 'last') as $position)
+    {
+      $this->getParameterHolder()->add($response->getStylesheets($position), 'helper/asset/auto/stylesheet'.$position);
+    }
+
+    // add javascripts
+    foreach (array('first', '', 'last') as $position)
+    {
+      $this->getParameterHolder()->add($response->getJavascripts($position), 'helper/asset/auto/javascript'.$position);
+    }
+
+    // add headers
+    foreach ($response->getHttpHeaders() as $name => $values)
+    {
+      foreach ($values as $value)
+      {
+        $this->setHttpHeader($name, $value);
+      }
+    }
+  }
+
+  public function __sleep()
+  {
+    return array('content', 'headers', 'statusCode', 'statusText', 'parameter_holder');
+  }
+
+  /**
+   * Execute the shutdown procedure.
+   *
+   * @return void
+   */
+  public function shutdown ()
+  {
+  }
+}
