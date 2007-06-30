55d7f9afb8a8baa80ce0e8602f85a01fa0bfbfe1
dwhittle <dwhittle@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2007-06-30 10:59:25 +0000

    dwhittle: adding support for pdo options, persistent, nulls, compatability
    git-svn-id: http://svn.symfony-project.com/branches/dwhittle@4495 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/database/sfPDODatabase.class.php
+++ lib/database/sfPDODatabase.class.php
@@ -4,7 +4,7 @@
  * This file is part of the symfony package.
  * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
  * (c) 2004-2006 Sean Kerr.
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -17,6 +17,7 @@
  * @author     Daniel Swarbrick (daniel@pressure.net.nz)
  * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
  * @author     Sean Kerr <skerr@mojavi.org>
+ * @author     Dustin Whittle <dustin.whittle@symfony-project.com>
  * @version    SVN: $Id$
  */
 class sfPDODatabase extends sfDatabase
@@ -50,10 +51,14 @@ class sfPDODatabase extends sfDatabase
 
     try
     {
-      $pdo_username = $this->getParameter('username');
-      $pdo_password = $this->getParameter('password');
+      $username = $this->getParameter('username');
+      $password = $this->getParameter('password');
 
-      $this->connection = new PDO($dsn, $pdo_username, $pdo_password);
+      $persistent = $this->getParameter('persistent');
+
+      $options = ($persistent) ? array(PDO::ATTR_PERSISTENT => true) : array(PDO::ATTR_PERSISTENT => false);
+
+      $this->connection = new PDO($dsn, $username, $password, $options);
     }
     catch (PDOException $e)
     {
@@ -61,13 +66,34 @@ class sfPDODatabase extends sfDatabase
     }
 
     // lets generate exceptions instead of silent failures
-    if (defined('PDO::ATTR_ERRMODE'))
+    if(sfConfig::get('sf_debug'))
     {
       $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     else
     {
-      $this->connection->setAttribute(PDO_ATTR_ERRMODE, PDO_ERRMODE_EXCEPTION);
+      $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+    }
+
+    // compatability
+    $compatability = $this->getParameter('compat');
+    if($compatability)
+    {
+      $this->connection->setAttribute(PDO::ATTR_CASE, PDO::CASE_NATURAL);
+    }
+
+    // nulls
+    $nulls = $this->getParameter('nulls');
+    if($nulls)
+    {
+      $this->connection->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
+    }
+
+    // auto commit
+    $autocommit = $this->getParameter('autocommit');
+    if($autocommit)
+    {
+      $this->connection->setAttribute(PDO::ATTR_AUTOCOMMIT, true);
     }
   }
 
