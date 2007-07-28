52ee588bb286bf3e49b1babe537b6d5e341f8eb8
dwhittle <dwhittle@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2007-07-28 05:56:12 +0000

    dwhittle: merge trunk changes to branch (r4717:4738)
    git-svn-id: http://svn.symfony-project.com/branches/dwhittle@4739 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/log/sfLogger/sfConsoleLogger.class.php
+++ lib/log/sfLogger/sfConsoleLogger.class.php
@@ -3,22 +3,29 @@
 /*
  * This file is part of the symfony package.
  * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
- * (c) 2004-2006 Sean Kerr.
  * 
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
 /**
- * sfStorageException is thrown when a requested sfStorage implementation doesn't
- * exist or data cannot be read from or written to the storage.
+ * sfConsoleLogger logs messages to the console.
  *
  * @package    symfony
- * @subpackage exception
+ * @subpackage log
  * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
- * @author     Sean Kerr <skerr@mojavi.org>
  * @version    SVN: $Id$
  */
-class sfStorageException extends sfException
+class sfConsoleLogger implements sfLoggerInterface
 {
+  /**
+   * Logs a message.
+   *
+   * @param string Message
+   * @param string Message priority
+   */
+  public function log($message, $priority = null)
+  {
+    echo $message;
+  }
 }
