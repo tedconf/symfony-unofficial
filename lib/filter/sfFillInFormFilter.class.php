8cf3ddb4f31e0d3128bc8765d13cb9451b81cc14
dwhittle <dwhittle@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2007-08-25 22:48:43 +0000

    dwhittle: fixed form fillin  to only run if content type is html and contains a form
    git-svn-id: http://svn.symfony-project.com/branches/dwhittle@4903 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/filter/sfFillInFormFilter.class.php
+++ lib/filter/sfFillInFormFilter.class.php
@@ -31,35 +31,38 @@ class sfFillInFormFilter extends sfFilter
     $response = $this->context->getResponse();
     $request  = $this->context->getRequest();
 
-    $fillInForm = new sfFillInForm();
-
-    // converters
-    foreach ($this->getParameter('converters', array()) as $functionName => $fields)
+    if(strpos($response->getContentType(), 'html') && strpos($content, '<form')) // only run if there is an html document and a form on the page
     {
-      $fillInForm->addConverter($functionName, $fields);
-    }
+      $fillInForm = new sfFillInForm();
 
-    // skip fields
-    $fillInForm->setSkipFields((array) $this->getParameter('skip_fields', array()));
-
-    // types
-    $excludeTypes = (array) $this->getParameter('exclude_types', array('hidden', 'password'));
-    $checkTypes   = (array) $this->getParameter('check_types',   array('text', 'checkbox', 'radio', 'password', 'hidden'));
-    $fillInForm->setTypes(array_diff($checkTypes, $excludeTypes));
-
-    // fill in
-    $method = 'fillIn'.ucfirst(strtolower($this->getParameter('content_type', 'Html')));
-
-    try
-    {
-      $content = $fillInForm->$method($response->getContent(), $this->getParameter('name'), $this->getParameter('id'), $request->getParameterHolder()->getAll());
-      $response->setContent($content);
-    }
-    catch (sfException $e)
-    {
-      if (sfConfig::get('sf_logging_enabled'))
+      // converters
+      foreach ($this->getParameter('converters', array()) as $functionName => $fields)
       {
-        $this->context->getLogger()->error(sprintf('{sfFilter} %s', $e->getMessage()));
+        $fillInForm->addConverter($functionName, $fields);
+      }
+
+      // skip fields
+      $fillInForm->setSkipFields((array) $this->getParameter('skip_fields', array()));
+
+      // types
+      $excludeTypes = (array) $this->getParameter('exclude_types', array('hidden', 'password'));
+      $checkTypes   = (array) $this->getParameter('check_types',   array('text', 'checkbox', 'radio', 'password', 'hidden'));
+      $fillInForm->setTypes(array_diff($checkTypes, $excludeTypes));
+
+      // fill in
+      $method = 'fillIn'.ucfirst(strtolower($this->getParameter('content_type', 'Html')));
+
+      try
+      {
+        $content = $fillInForm->$method($response->getContent(), $this->getParameter('name'), $this->getParameter('id'), $request->getParameterHolder()->getAll());
+        $response->setContent($content);
+      }
+      catch (sfException $e)
+      {
+        if (sfConfig::get('sf_logging_enabled'))
+        {
+          $this->context->getLogger()->error(sprintf('{sfFilter} %s', $e->getMessage()));
+        }
       }
     }
   }
