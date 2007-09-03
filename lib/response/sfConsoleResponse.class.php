02b7d82de11188f2f9ab1ceba8f475b077d5b4ff
dwhittle <dwhittle@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2007-09-03 11:15:11 +0000

    dwhittle: added serialize/unserialize to sfConsoleResponse
    git-svn-id: http://svn.symfony-project.com/branches/dwhittle@4965 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/response/sfConsoleResponse.class.php
+++ lib/response/sfConsoleResponse.class.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the symfony package.
  * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -18,4 +18,27 @@
  */
 class sfConsoleResponse extends sfResponse
 {
+
+  /**
+   * Serializes the current instance.
+   *
+   * @return array Objects instance
+   */
+  public function serialize()
+  {
+    return serialize(array($this->content, $this->parameterHolder));
+  }
+
+  /**
+   * Unserializes a sfConsoleResponse instance.
+   */
+  public function unserialize($serialized)
+  {
+    $data = unserialize($serialized);
+
+    $this->initialize(sfContext::hasInstance() ? sfContext::getInstance()->getEventDispatcher() : new sfEventDispatcher());
+
+    $this->content         = $data[0];
+    $this->parameterHolder = $data[1];
+  }
 }
