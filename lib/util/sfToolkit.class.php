ca560f428756f6ff38599731eb0982c0cecaf315
dwhittle <dwhittle@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2007-10-01 07:29:38 +0000

    dwhittle: reverted sfToolkit::isUTF8 speed improvement fix
    git-svn-id: http://svn.symfony-project.com/branches/dwhittle@5338 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/util/sfToolkit.class.php
+++ lib/util/sfToolkit.class.php
@@ -411,18 +411,48 @@ class sfToolkit
    */
   public static function isUTF8($string)
   {
-    if (strlen($string) == 0)
+    for ($idx = 0, $strlen = strlen($string); $idx < $strlen; $idx++)
     {
-        return true;
+      $byte = ord($string[$idx]);
+
+      if ($byte & 0x80)
+      {
+        if (($byte & 0xE0) == 0xC0)
+        {
+          // 2 byte char
+          $bytes_remaining = 1;
+        }
+        else if (($byte & 0xF0) == 0xE0)
+        {
+          // 3 byte char
+          $bytes_remaining = 2;
+        }
+        else if (($byte & 0xF8) == 0xF0)
+        {
+          // 4 byte char
+          $bytes_remaining = 3;
+        }
+        else
+        {
+          return false;
+        }
+
+        if ($idx + $bytes_remaining >= $strlen)
+        {
+          return false;
+        }
+
+        while ($bytes_remaining--)
+        {
+          if ((ord($string[++$idx]) & 0xC0) != 0x80)
+          {
+            return false;
+          }
+        }
+      }
     }
 
-    /*
-     * If even just the first character can be matched, when the /u
-     * modifier is used, then it's valid UTF-8. If the UTF-8 is somehow
-     * invalid, nothing at all will match, even if the string contains
-     * some valid sequences
-     */
-    return (preg_match('/^.{1}/us', $string, $array) == 1);
+    return true;
   }
 
   public static function getArrayValueForPath($values, $name, $default = null)
