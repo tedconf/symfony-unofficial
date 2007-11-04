e828ed2d554f2239222201731d8ace168d49d812
dwhittle <dwhittle@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2007-11-04 22:42:08 +0000

    dwhittle: merge trunk changes to branch
    git-svn-id: http://svn.symfony-project.com/branches/dwhittle@5863 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfCompat10Plugin/lib/helper/JavascriptHelper.php
+++ lib/plugins/sfCompat10Plugin/lib/helper/JavascriptHelper.php
@@ -905,7 +905,7 @@
     }
     if (isset ($options['with']))
     {
-      $js_options['callback'] = "function(element, value) { return".$options['with']."}";
+      $js_options['callback'] = "function(element, value) { return ".$options['with']."}";
     }
     if (isset($options['indicator']))
     {
