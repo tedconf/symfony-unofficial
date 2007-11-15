3eebf480c93d4a44479f6cb00a7d0db0fa12cdb0
dwhittle <dwhittle@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2007-11-15 08:07:46 +0000

    dwhittle: fixed error constant in filter (non-bc)
    git-svn-id: http://svn.symfony-project.com/branches/dwhittle@6039 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfCompat10Plugin/lib/filter/sfFillInFormFilter.class.php
+++ lib/plugins/sfCompat10Plugin/lib/filter/sfFillInFormFilter.class.php
@@ -59,7 +59,7 @@ class sfFillInFormFilter extends sfFilter
     {
       if (sfConfig::get('sf_logging_enabled'))
       {
-        $this->context->getEventDispatcher()->notify(new sfEvent($this, 'application.log', array($e->getMessage(), 'priority' => sfLogger::ERR)));
+        $this->context->getEventDispatcher()->notify(new sfEvent($this, 'application.log', array($e->getMessage(), 'priority' => sfLogger::ERROR)));
       }
     }
   }
