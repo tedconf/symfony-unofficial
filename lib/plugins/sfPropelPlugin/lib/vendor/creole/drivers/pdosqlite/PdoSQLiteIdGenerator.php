706f8ef0916744885a4dc2f2e56be29422cfc74f
dwhittle <dwhittle@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2007-11-27 19:37:19 +0000

    dwhittle: imported creole 1.2 (with all fixes + patch) and jargon for propel 1.3 	   + required for reverse engineering schema from existing database
    git-svn-id: http://svn.symfony-project.com/branches/dwhittle@6183 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/plugins/sfPropelPlugin/lib/vendor/creole/drivers/pdosqlite/PdoSQLiteIdGenerator.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'creole/Common/PdoIdGeneratorCommon.php';
+
+/**
+ * SQLite IdGenerator implimenation.
+ *
+ * @author    Hans Lellelid <hans@xmpl.org>
+ * @version   $Revision: 1.4 $
+ * @package   creole.drivers.sqlite
+ */
+class PdoSQLiteIdGenerator extends PdoIdGeneratorCommon {
+    
+}
+
