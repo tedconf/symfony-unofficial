d8f7a1fa2e10ef37163a125f10c45f48ac0d8156
dwhittle <dwhittle@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2007-12-01 09:35:32 +0000

    dwhittle: set permissions at creation time for sfFileLogger
    git-svn-id: http://svn.symfony-project.com/branches/dwhittle@6254 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/log/sfFileLogger.class.php
+++ lib/log/sfFileLogger.class.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the symfony package.
  * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -47,12 +47,18 @@ class sfFileLogger extends sfLogger
       mkdir($dir, 0777, true);
     }
 
-    if (!is_writable($dir) || (file_exists($options['file']) && !is_writable($options['file'])))
+    $file_exists = file_exists($options['file']);
+    if (!is_writable($dir) || ($file_exists && !is_writable($options['file'])))
     {
       throw new sfFileException(sprintf('Unable to open the log file "%s" for writing.', $options['file']));
     }
 
     $this->fp = fopen($options['file'], 'a');
+
+    if (!$file_exists)
+    {
+      chmod($options['file'], 0666);
+    }
 
     return parent::initialize($dispatcher, $options);
   }
