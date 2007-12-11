a50333dd8602c315b81e10045e9904feb1ecafb0
dwhittle <dwhittle@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2007-12-11 10:30:05 +0000

    dwhittle: catch propel exception and display error message for admin generated edit actions (similar to delete)
    git-svn-id: http://svn.symfony-project.com/branches/dwhittle@6449 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfPropelPlugin/data/generator/sfPropelAdmin/default/template/templates/_list_messages.php
+++ lib/plugins/sfPropelPlugin/data/generator/sfPropelAdmin/default/template/templates/_list_messages.php
@@ -6,3 +6,12 @@
   </ul>
 </div>
 [?php endif; ?]
+
+[?php if ($sf_request->hasError('edit')): ?]
+<div class="form-errors">
+  <h2>[?php echo __('Could not edit %name%', array('%name%' => '<?php echo sfInflector::humanize($this->getSingularName()) ?>')) ?]</h2>
+  <ul>
+    <li>[?php echo __($sf_request->getError('edit')) ?]</li>
+  </ul>
+</div>
+[?php endif; ?]
