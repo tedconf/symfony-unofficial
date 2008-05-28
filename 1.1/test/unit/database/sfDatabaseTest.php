d49eed539303f9d9e97779658c688a8be7702565
dwhittle <dwhittle@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-05-28 19:37:34 +0000

    dwhittle: merged changes to branch
    git-svn-id: http://svn.symfony-project.com/branches/dwhittle@9348 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- 1.1/test/unit/database/sfDatabaseTest.php
+++ 1.1/test/unit/database/sfDatabaseTest.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the symfony package.
  * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -19,10 +19,7 @@ class myDatabase extends sfDatabase
   function shutdown () {}
 }
 
-$context = sfContext::getInstance();
-
-$database = new myDatabase();
-$database->initialize($context);
+$database = new myDatabase(sfContext::getInstance());
 
 // parameter holder proxy
 require_once($_test_dir.'/unit/sfParameterHolderTest.class.php');
