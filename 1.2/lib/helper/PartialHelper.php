941cdaf0de5f62df16ffc1bf463756ae63d633bc
dwhittle <dwhittle@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-09-12 18:02:19 +0000

    dwhittle: merged changes to branch
    git-svn-id: http://svn.symfony-project.com/branches/dwhittle@11485 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- 1.2/lib/helper/PartialHelper.php
+++ 1.2/lib/helper/PartialHelper.php
@@ -136,7 +136,8 @@ function get_component($moduleName, $componentName, $vars = array())
   $context = sfContext::getInstance();
   $actionName = '_'.$componentName;
 
-  $view = new sfPartialView($context, $moduleName, $actionName, '');
+  $class = sfConfig::get('mod_'.$moduleName.'_partial_view_class', 'sf').'PartialView';
+  $view = new $class($context, $moduleName, $actionName, '');
   $view->setPartialVars($vars);
 
   if ($retval = $view->getCache())
@@ -210,7 +211,8 @@ function get_partial($templateName, $vars = array())
   }
   $actionName = '_'.$templateName;
 
-  $view = new sfPartialView($context, $moduleName, $actionName, '');
+  $class = sfConfig::get('mod_'.$moduleName.'_partial_view_class', 'sf').'PartialView';
+  $view = new $class($context, $moduleName, $actionName, '');
   $view->setPartialVars($vars);
 
   return $view->render();
