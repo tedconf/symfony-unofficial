54163856c62f04afffbc7feb8ba804b95e8ab4de
dwhittle <dwhittle@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-12-24 04:42:43 +0000

    dwhittle: fixed test count
    git-svn-id: http://svn.symfony-project.com/branches/dwhittle@14298 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- 1.2/test/unit/form/sfFormTest.php
+++ 1.2/test/unit/form/sfFormTest.php
@@ -10,7 +10,7 @@
 
 require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
 
-$t = new lime_test(133, new lime_output_color());
+$t = new lime_test(120, new lime_output_color());
 
 class FormTest extends sfForm
 {
