d2de183300007bc70d2a177af213761dfb4bc15d
dwhittle <dwhittle@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-01-17 10:25:36 +0000

    dwhittle: merged changes to branch r14704:14833
    git-svn-id: http://svn.symfony-project.com/branches/dwhittle@14836 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- 1.3/lib/plugins/sfDoctrinePlugin/lib/pager/sfDoctrinePager.class.php
+++ 1.3/lib/plugins/sfDoctrinePlugin/lib/pager/sfDoctrinePager.class.php
@@ -123,6 +123,8 @@ class sfDoctrinePager extends sfPager implements Serializable
       $method = $this->tableMethodName;
       $this->query = Doctrine::getTable($this->getClass())->$method($this->query);
       $this->tableMethodCalled = true;
+    } else if (!$this->query) {
+      $this->query = Doctrine::getTable($this->getClass())->createQuery();
     }
     return $this->query;
   }
