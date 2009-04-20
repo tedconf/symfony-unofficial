696c6317d878aa0c47fe242477241d48869d1b9b
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-04-20 10:55:24 +0000

    [1.2, 1.3] reverted r17338, applied the other patch from #5811 with the compat option
    git-svn-id: http://svn.symfony-project.com/branches/1.2@17440 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/unit/routing/sfPatternRoutingTest.php
+++ test/unit/routing/sfPatternRoutingTest.php
@@ -509,7 +509,7 @@ $rCached = new sfPatternRoutingTest($dispatcher, $cache, array_merge($options, a
 $rCached->parse('/first');
 $t->isnt($rCached->findRoute('/first'), false, '->findRoute() finds the route with lazy config cache activated');
 $t->is($rCached->isRouteLoaded('second'), false, '->isRouteLoaded() The second route is not loaded');
-$t->is($rCached->findRoute('/no/match/found2'), null, '->findRoute() returns null on non-matching route');
+$t->is($rCached->findRoute('/no/match/found'), null, '->findRoute() returns null on non-matching route');
 $t->is($rCached->findRoute('/no/match/found'), null, '->findRoute() returns null on non-matching route');
 $t->is($rCached->isRouteLoaded('second'), true, '->isRouteLoaded() The last route is loaded after a full routes scan');
 
