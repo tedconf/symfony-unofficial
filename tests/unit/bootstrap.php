901b4bce4e6cb9183ba7f31e9c5a0815240ff7ec
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-12-23 09:46:02 +0000

    [2.0] fixed tests
    git-svn-id: http://svn.symfony-project.com/branches/2.0@25812 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -12,5 +12,6 @@ require_once __DIR__.'/../lib/vendor/lime/LimeAutoloader.php';
 LimeAutoloader::register();
 
 require_once __DIR__.'/../../lib/Symfony/Foundation/ClassLoader.php';
-$loader = new Symfony\Foundation\ClassLoader('Symfony', __DIR__.'/../../lib');
+$loader = new Symfony\Foundation\ClassLoader();
+$loader->registerNamespace('Symfony', __DIR__.'/../../lib');
 $loader->register();
