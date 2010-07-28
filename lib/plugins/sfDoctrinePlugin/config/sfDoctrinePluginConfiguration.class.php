a9a90c05a782d56413d842f2a6890ab19e8bd7a5
Kris.Wallsmith <Kris.Wallsmith@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-07-28 04:37:32 +0000

    [1.3, 1.4] added a check whether doctrine is already loaded to allow use of a compiled core (closes #8917, thanks gnukix)
    git-svn-id: http://svn.symfony-project.com/branches/1.3@30445 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfDoctrinePlugin/config/sfDoctrinePluginConfiguration.class.php
+++ lib/plugins/sfDoctrinePlugin/config/sfDoctrinePluginConfiguration.class.php
@@ -50,7 +50,10 @@ class sfDoctrinePluginConfiguration extends sfPluginConfiguration
       }
     }
 
-    require_once sfConfig::get('sf_doctrine_dir').'/Doctrine/Core.php';
+    if (!class_exists('Doctrine_Core', false))
+    {
+      require_once sfConfig::get('sf_doctrine_dir').'/Doctrine/Core.php';
+    }
     spl_autoload_register(array('Doctrine_Core', 'autoload'));
 
     $manager = Doctrine_Manager::getInstance();
